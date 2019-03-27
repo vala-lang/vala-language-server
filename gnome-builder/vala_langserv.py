26cce4359991840bd87c0e959f8e3537a4e8a4d5
Prince781 <princetonf@optonline.net>
2019-03-26 21:08:57 -0400

    Fix plugin.

--- gnome-builder/vala_langserv.py
+++ gnome-builder/vala_langserv.py
@@ -1,9 +1,9 @@
+
 #!/usr/bin/env python
 
-# vala-langserv.py
+#   vala.py
 #
 # Copyright 2016 Christian Hergert <chergert@redhat.com>
-# Copyright 2019 Princeton Ferro <princetonferro@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -27,8 +27,6 @@ by bridging them to our supervised Vala Language Server.
 import gi
 import os
 
-gi.require_version('Ide', '1.0')
-
 from gi.repository import GLib
 from gi.repository import Gio
 from gi.repository import GObject
@@ -36,13 +34,17 @@ from gi.repository import Ide
 
 DEV_MODE = False
 
-class ValaService(Ide.Object, Ide.Service):
+class VlsService(Ide.Object):
     _client = None
     _has_started = False
     _supervisor = None
     _monitor = None
 
-    @GObject.Property(type=Ide.LangservClient)
+    @classmethod
+    def from_context(klass, context):
+        return context.ensure_child_typed(VlsService)
+
+    @GObject.Property(type=Ide.LspClient)
     def client(self):
         return self._client
 
@@ -51,26 +53,30 @@ class ValaService(Ide.Object, Ide.Service):
         self._client = value
         self.notify('client')
 
-    def do_context_loaded(self):
+    def do_parent_set(self, parent):
         """
         After the context has been loaded, we want to watch the project
         meson.build for changes if we find one. That will allow us to
         restart the process as necessary to pick up changes.
         """
+        if parent is None:
+            return
+
         context = self.get_context()
-        project_file = context.get_project_file()
-        if project_file is not None:
-            if project_file.get_basename() == 'meson.build':
-                try:
-                    self._monitor = project_file.monitor(0, None)
-                    self._monitor.set_rate_limit(5 * 1000) # 5 Seconds
-                    self._monitor.connect('changed', self._monitor_changed_cb)
-                except Exception as ex:
-                    Ide.debug('Failed to monitor meson.build for changes:', repr(ex))
+        workdir = context.ref_workdir()
+        meson_build = workdir.get_child('meson.build')
+
+        if meson_build.query_exists():
+            try:
+                self._monitor = meson_build.monitor(0, None)
+                self._monitor.set_rate_limit(5 * 1000) # 5 Seconds
+                self._monitor.connect('changed', self._monitor_changed_cb)
+            except Exception as ex:
+                Ide.debug('Failed to monitor meson.build for changes:', repr(ex))
 
     def _monitor_changed_cb(self, monitor, file, other_file, event_type):
         """
-        This method is called when Cargo.toml has changed. We need to
+        This method is called when meson.build has changed. We need to
         cancel any supervised process and force the language server to
         restart. Otherwise, we risk it not picking up necessary changes.
         """
@@ -82,7 +88,7 @@ class ValaService(Ide.Object, Ide.Service):
     def do_stop(self):
         """
         Stops the Vala Language Server upon request to shutdown the
-        ValaService.
+        VlsService.
         """
         if self._monitor is not None:
             monitor, self._monitor = self._monitor, None
@@ -95,58 +101,63 @@ class ValaService(Ide.Object, Ide.Service):
 
     def _ensure_started(self):
         """
-        Start the vala service which provides communication with the
+        Start the rust service which provides communication with the
         Vala Language Server. We supervise our own instance of the
         language server and restart it as necessary using the
         Ide.SubprocessSupervisor.
 
         Various extension points (diagnostics, symbol providers, etc) use
-        the ValaService to access the vala components they need.
+        the VlsService to access the rust components they need.
         """
-        # To avoid starting the `rls` process unconditionally at startup,
+        # To avoid starting the `vls` process unconditionally at startup,
         # we lazily start it when the first provider tries to bind a client
         # to its :client property.
         if not self._has_started:
             self._has_started = True
 
-            # Setup a launcher to spawn the vala language server
+            # Setup a launcher to spawn the rust language server
             launcher = self._create_launcher()
             launcher.set_clear_env(False)
-            # sysroot = self._discover_sysroot()
-            # if sysroot:
-            #     launcher.setenv("SYS_ROOT", sysroot, True)
-            #     launcher.setenv("LD_LIBRARY_PATH", os.path.join(sysroot, "lib"), True)
+            sysroot = self._discover_sysroot()
+            if sysroot:
+                launcher.setenv("SYS_ROOT", sysroot, True)
+                launcher.setenv("LD_LIBRARY_PATH", os.path.join(sysroot, "lib"), True)
             if DEV_MODE:
                 launcher.setenv('G_MESSAGES_DEBUG', 'all', True)
 
-            # Locate the directory of the project and run rls from there.
-            workdir = self.get_context().get_vcs().get_working_directory()
+            # Locate the directory of the project and run vls from there.
+            workdir = self.get_context().ref_workdir()
             launcher.set_cwd(workdir.get_path())
 
-            # If rls was installed with Cargo, try to discover that
+            # If vls was installed with Cargo, try to discover that
             # to save the user having to update PATH.
-            # path_to_rls = os.path.expanduser("~/.cargo/bin/rls")
-            # if os.path.exists(path_to_rls):
-            #     launcher.setenv('PATH', os.path.expanduser("~/.cargo/bin"), True)
-            # else:
-            path_to_rls = "vala-language-server"
+#             path_to_vls = os.path.expanduser("~/.cargo/bin/vls")
+#             if os.path.exists(path_to_vls):
+#                 old_path = os.getenv('PATH')
+#                 new_path = os.path.expanduser('~/.cargo/bin')
+#                 if old_path is not None:
+#                     new_path += os.path.pathsep + old_path
+#                 launcher.setenv('PATH', new_path, True)
+#             else:
+#                 path_to_vls = "vls"
+            path_to_vls = "vala-language-server"
 
             # Setup our Argv. We want to communicate over STDIN/STDOUT,
             # so it does not require any command line options.
-            launcher.push_argv(path_to_rls)
+            launcher.push_argv(path_to_vls)
 
             # Spawn our peer process and monitor it for
             # crashes. We may need to restart it occasionally.
             self._supervisor = Ide.SubprocessSupervisor()
-            self._supervisor.connect('spawned', self._rls_spawned)
+            self._supervisor.connect('spawned', self._vls_spawned)
             self._supervisor.set_launcher(launcher)
             self._supervisor.start()
 
-    def _rls_spawned(self, supervisor, subprocess):
+    def _vls_spawned(self, supervisor, subprocess):
         """
-        This callback is executed when the `rls` process is spawned.
+        This callback is executed when the `vls` process is spawned.
         We can use the stdin/stdout to create a channel for our
-        LangservClient.
+        LspClient.
         """
         stdin = subprocess.get_stdin_pipe()
         stdout = subprocess.get_stdout_pipe()
@@ -154,8 +165,10 @@ class ValaService(Ide.Object, Ide.Service):
 
         if self._client:
             self._client.stop()
+            self._client.destroy()
 
-        self._client = Ide.LangservClient.new(self.get_context(), io_stream)
+        self._client = Ide.LspClient.new(io_stream)
+        self.append(self._client)
         self._client.add_language('vala')
         self._client.start()
         self.notify('client')
@@ -179,66 +192,68 @@ class ValaService(Ide.Object, Ide.Service):
         launcher.set_run_on_host(True)
         return launcher
 
-    def _discover_sysroot(self):            # currently unused
+    def _discover_sysroot(self):
         """
-        The Rust Language Server needs to know where the sysroot is of
-        the Rust installation we are using. This is simple enough to
+        The Vala Language Server needs to know where the sysroot is of
+        the Vala installation we are using. This is simple enough to
         get, by using `rust --print sysroot` as the rust-language-server
         documentation suggests.
         """
-        for rustc in ['rustc', os.path.expanduser('~/.cargo/bin/rustc')]:
-            try:
-                launcher = self._create_launcher()
-                launcher.push_args([rustc, '--print', 'sysroot'])
-                subprocess = launcher.spawn()
-                _, stdout, _ = subprocess.communicate_utf8()
-                return stdout.strip()
-            except:
-                pass
+
+        pass
+#        for valac in ['valac', os.path.expanduser('~/.cargo/bin/valac')]:
+#            try:
+#                launcher = self._create_launcher()
+#                launcher.push_args([valac, '--print', 'sysroot'])
+#                subprocess = launcher.spawn()
+#                _, stdout, _ = subprocess.communicate_utf8()
+#                return stdout.strip()
+#            except:
+#                pass
 
     @classmethod
     def bind_client(klass, provider):
         """
         This helper tracks changes to our client as it might happen when
-        our `rls` process has crashed.
+        our `vls` process has crashed.
         """
         context = provider.get_context()
-        self = context.get_service_typed(ValaService)
+        self = VlsService.from_context(context)
         self._ensure_started()
         self.bind_property('client', provider, 'client', GObject.BindingFlags.SYNC_CREATE)
 
-# TODO: get it to work with just this
-class ValaDiagnosticProvider(Ide.LangservDiagnosticProvider):
+class VlsDiagnosticProvider(Ide.LspDiagnosticProvider):
     def do_load(self):
-        ValaService.bind_client(self)
+        VlsService.bind_client(self)
 
-# class ValaCompletionProvider(Ide.LangservCompletionProvider):
-#     def do_load(self, context):
-#         ValaService.bind_client(self)
-# 
-#     def do_get_priority(self, context):
-#         # This provider only activates when it is very likely that we
-#         # want the results. So use high priority (negative is better).
-#         return -1000
-# 
-# class ValaRenameProvider(Ide.LangservRenameProvider):
-#     def do_load(self):
-#         ValaService.bind_client(self)
-# 
-# class ValaSymbolResolver(Ide.LangservSymbolResolver):
-#     def do_load(self):
-#         ValaService.bind_client(self)
-# 
-# class ValaHighlighter(Ide.LangservHighlighter):
-#     def do_load(self):
-#         ValaService.bind_client(self)
-# 
-# class ValaFormatter(Ide.LangservFormatter):
-#     def do_load(self):
-#         ValaService.bind_client(self)
-# 
-# class ValaHoverProvider(Ide.LangservHoverProvider):
-#     def do_prepare(self):
-#         self.props.category = 'Vala'
-#         self.props.priority = 200
-#         ValaService.bind_client(self)
+class VlsCompletionProvider(Ide.LspCompletionProvider):
+    def do_load(self, context):
+        VlsService.bind_client(self)
+
+    def do_get_priority(self, context):
+        # This provider only activates when it is very likely that we
+        # want the results. So use high priority (negative is better).
+        return -1000
+
+class VlsRenameProvider(Ide.LspRenameProvider):
+    def do_load(self):
+        VlsService.bind_client(self)
+
+class VlsSymbolResolver(Ide.LspSymbolResolver):
+    def do_load(self):
+        VlsService.bind_client(self)
+
+class VlsHighlighter(Ide.LspHighlighter):
+    def do_load(self):
+        VlsService.bind_client(self)
+
+class VlsFormatter(Ide.LspFormatter):
+    def do_load(self):
+        VlsService.bind_client(self)
+
+class VlsHoverProvider(Ide.LspHoverProvider):
+    def do_prepare(self):
+        self.props.category = 'Vala'
+        self.props.priority = 200
+        VlsService.bind_client(self)
+
