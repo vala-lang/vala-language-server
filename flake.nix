133b2d9c3ae93be4a3527f789ea529b143ef8f0d
Ben Iofel <ben@flow.io>
2022-10-22 23:52:21 +0200

    Use nix flake for dependencies

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  outputs = { self, nixpkgs }:
+    let
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 self.lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ pkg-config meson ninja vala jsonrpc-glib json-glib libgee glib ];
+          };
+        });
+
+    };
+
+}
